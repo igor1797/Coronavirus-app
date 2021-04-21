402a0f55d1550179fed67d930a627e3cfcd16dc3
igor1797 <48154245+igor1797@users.noreply.github.com>
2021-04-21 21:07:28 +0200

    Created dark and light theme design for Page coming soon screen. Added Lottie library dependency and used for animation.

---
+++ app/src/main/java/hr/dice/coronavirus/app/ui/page_coming_soon/PageComingSoonFragment.kt
@@ -0,0 +1,13 @@
+package hr.dice.coronavirus.app.ui.page_coming_soon
+
+import hr.dice.coronavirus.app.R
+import hr.dice.coronavirus.app.databinding.FragmentPageComingSoonBinding
+import hr.dice.coronavirus.app.ui.base.BaseFragment
+
+class PageComingSoonFragment : BaseFragment<FragmentPageComingSoonBinding>() {
+
+    override val layoutResourceId: Int get() = R.layout.fragment_page_coming_soon
+
+    override fun onPostViewCreated() {
+    }
+}
