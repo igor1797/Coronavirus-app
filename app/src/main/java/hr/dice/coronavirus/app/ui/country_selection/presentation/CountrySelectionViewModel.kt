edb9650afcabcf64fce039a49b8ecf1acd3c6e15
igor1797 <48154245+igor1797@users.noreply.github.com>
2021-04-22 20:56:01 +0200

    Created dark and light theme design for Country selection screen and country recycler item.

---
+++ app/src/main/java/hr/dice/coronavirus/app/ui/country_selection/presentation/CountrySelectionViewModel.kt
@@ -0,0 +1,7 @@
+package hr.dice.coronavirus.app.ui.country_selection.presentation
+
+import androidx.lifecycle.ViewModel
+
+class CountrySelectionViewModel : ViewModel() {
+
+}
---
+++ app/src/main/java/hr/dice/coronavirus/app/ui/country_selection/view/CountrySelectionFragment.kt
@@ -0,0 +1,13 @@
+package hr.dice.coronavirus.app.ui.country_selection.view
+
+import hr.dice.coronavirus.app.R
+import hr.dice.coronavirus.app.databinding.CountrySelectionFragmentBinding
+import hr.dice.coronavirus.app.ui.base.BaseFragment
+
+class CountrySelectionFragment : BaseFragment<CountrySelectionFragmentBinding>() {
+
+    override val layoutResourceId: Int get() = R.layout.country_selection_fragment
+
+    override fun onPostViewCreated() {
+    }
+}
