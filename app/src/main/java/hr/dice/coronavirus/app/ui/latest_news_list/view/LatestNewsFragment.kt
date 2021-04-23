4c2b45e8b8545f1d2f3cea7bdf3dba6affecdb02
igor1797 <48154245+igor1797@users.noreply.github.com>
2021-04-23 15:46:20 +0200

    Created dark and light theme design for Latest news list screen and single news item.

---
+++ app/src/main/java/hr/dice/coronavirus/app/ui/latest_news_list/view/LatestNewsFragment.kt
@@ -0,0 +1,13 @@
+package hr.dice.coronavirus.app.ui.latest_news_list.view
+
+import hr.dice.coronavirus.app.R
+import hr.dice.coronavirus.app.databinding.FragmentLatestNewsBinding
+import hr.dice.coronavirus.app.ui.base.BaseFragment
+
+class LatestNewsFragment : BaseFragment<FragmentLatestNewsBinding>() {
+
+    override val layoutResourceId: Int get() = R.layout.fragment_latest_news
+
+    override fun onPostViewCreated() {
+    }
+}
