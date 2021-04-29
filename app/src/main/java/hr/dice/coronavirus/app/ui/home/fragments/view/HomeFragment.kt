1e719a3f571cad8e8cf6c77ab2c914ae94f51c44
Igor Kuridža <kuridza.igor@gmail.com>
2021-04-29 16:22:49 +0000

    #10 Feature - Home container view

---
+++ app/src/main/java/hr/dice/coronavirus/app/ui/home/fragments/view/HomeFragment.kt
@@ -0,0 +1,13 @@
+package hr.dice.coronavirus.app.ui.home.fragments.view
+
+import hr.dice.coronavirus.app.R
+import hr.dice.coronavirus.app.databinding.FragmentHomeBinding
+import hr.dice.coronavirus.app.ui.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+
+    override val layoutResourceId: Int get() = R.layout.fragment_home
+
+    override fun onPostViewCreated() {
+    }
+}
