339ba3ce0c912c7e6e00547533f0f35f23a6a58b
igor1797 <48154245+igor1797@users.noreply.github.com>
2021-05-19 15:08:00 +0200

    Removed layouts for dark theme. Extracted strings from DateTimeUtil class to strings file.

--- app/src/main/java/hr/dice/coronavirus/app/common/utils/DateTimeUtil.kt
+++ app/src/main/java/hr/dice/coronavirus/app/common/utils/DateTimeUtil.kt
@@ -1,5 +1,7 @@
 package hr.dice.coronavirus.app.common.utils
 
+import android.app.Application
+import hr.dice.coronavirus.app.R
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
@@ -7,7 +9,8 @@ import java.util.TimeZone
 
 class DateTimeUtil(
     private val simpleDateFormat: SimpleDateFormat,
-    private val calendar: Calendar
+    private val calendar: Calendar,
+    private val context: Application
 ) {
 
     private fun convertTimeInMillis(
@@ -26,17 +29,17 @@ class DateTimeUtil(
         val currentTimeInMillis = calendar.timeInMillis
         val timeAgoInSeconds = ((currentTimeInMillis - convertTimeInMillis(dateTimeFormat, timeZone, time)) / 1000)
         return when (timeAgoInSeconds) {
-            in 0..59 -> "now"
-            in 60..119 -> "1 min ago"
-            in 119..3599 -> "${timeAgoInSeconds / 60} mins ago"
-            in 3600..7199 -> "1 hour ago"
-            in 7200..86399 -> "${timeAgoInSeconds / 3600} hours ago"
-            in 86400..172799 -> "1 day ago"
-            in 172800..2629742 -> "${timeAgoInSeconds / 86400} days ago"
-            in 2629743..5259486 -> "1 month ago"
-            in 5259487..31556926 -> "${timeAgoInSeconds / 2629743} months ago"
-            in 31556926..63113851 -> "1 year ago"
-            else -> "${timeAgoInSeconds / 31556926} years ago"
+            in 0..59 -> context.getString(R.string.nowText)
+            in 60..119 -> context.getString(R.string.oneMinuteAgoText)
+            in 119..3599 -> context.getString(R.string.minsAgoText, (timeAgoInSeconds / 60).toString())
+            in 3600..7199 -> context.getString(R.string.oneHourAgoText)
+            in 7200..86399 -> context.getString(R.string.hoursAgoText, (timeAgoInSeconds / 3600).toString())
+            in 86400..172799 -> context.getString(R.string.oneDayAgoText)
+            in 172800..2629742 -> context.getString(R.string.daysAgoText, (timeAgoInSeconds / 86400).toString())
+            in 2629743..5259486 -> context.getString(R.string.oneMonthAgoText)
+            in 5259487..31556926 -> context.getString(R.string.monthsAgoText, (timeAgoInSeconds / 2629743).toString())
+            in 31556926..63113851 -> context.getString(R.string.oneYearAgoText)
+            else -> context.getString(R.string.yearsAgoText, (timeAgoInSeconds / 31556926).toString())
         }
     }
 }
