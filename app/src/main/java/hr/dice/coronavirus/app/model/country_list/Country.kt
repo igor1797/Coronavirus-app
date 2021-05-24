a9c809cfad27fe42574ea581e46667e6b45045ec
igor1797 <48154245+igor1797@users.noreply.github.com>
2021-05-24 16:09:55 +0200

    Removed Country extension for getting country emoji to other file

--- app/src/main/java/hr/dice/coronavirus/app/model/country_list/Country.kt
+++ app/src/main/java/hr/dice/coronavirus/app/model/country_list/Country.kt
@@ -5,14 +5,3 @@ data class Country(
     val slug: String,
     val iso2: String
 )
-
-fun Country.getFlagEmoji(): String {
-    val firstLetter = Character.codePointAt(iso2, 0) - 0x41 + 0x1F1E6
-    val secondLetter = Character.codePointAt(iso2, 1) - 0x41 + 0x1F1E6
-
-    if (!iso2[0].isLetter() || !iso2[1].isLetter()) {
-        return iso2
-    }
-
-    return String(Character.toChars(firstLetter)) + String(Character.toChars(secondLetter))
-}
